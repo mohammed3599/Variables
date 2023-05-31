c4ca3d78f3319a1fa7aaeed1e96e38559bfe3c94
mohammed3599 <kataloni1999@gmail.com>
2023-05-31 11:23:17 +0400

    string concatenation

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 1;
+           char x = 'a';
+           double p = 2.3;
+           string y = "hello";
+           bool z = true;
+            /*Console.WriteLine(a);
+            Console.WriteLine(y);
+            Console.WriteLine(z);*/
+            Console.WriteLine(a +""+ x);
+            Console.WriteLine(p +""+ a);
+            
+        }
+    }
+}
